6a4f1874b4a4d70b2c00f46d05806af353827d6c
Olga Wu <54147141+qaolgawu@users.noreply.github.com>
2024-06-25 12:29:24 -0400

    edited

--- public/js/user.js
+++
@@ -1,8 +0,0 @@
-class User {
-    setUser = ({name, email, avatar}) => {
-        this.name = name
-        this.email = email
-        this.avatar = avatar
-    }    
-}
-export const user = new User()
